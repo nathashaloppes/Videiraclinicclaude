5dff5bb76c0175abd64a22897ce0a5dd72afad6d
Ianderson Farias <iandersoncounter@gmail.com>
2026-06-15 17:21:50 -0300

    Expira reservas Pix mais perto dos 30min (job a cada 1min)
    Co-Authored-By: nathashaloppes <nathashalopes06@icloud.com>

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -2,7 +2,7 @@ Sidekiq.configure_server do |config|
   config.on(:startup) do
     Sidekiq::Cron::Job.load_from_hash(
       "expire_payments" => {
-        "cron"  => "*/5 * * * *",
+        "cron"  => "* * * * *",
         "class" => "ExpirePaymentsJob"
       }
     )
